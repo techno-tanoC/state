cbb8da3baf20b232cae280e5fb3ad3681807446f
techno-tanoC <develop.tanoc@gmail.com>
2016-05-28 12:47:18 +0900

    update

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,19 @@
 module Main where
 
-import Lib
+import Control.Monad.Trans.State
+
+addOne :: State Int ()
+addOne = modify succ
+
+doubleEven :: State Int ()
+doubleEven = do
+  n <- get
+  if even n
+  then put $ n * 2
+  else return ()
 
 main :: IO ()
-main = return ()
+main = do
+  print $ execState (addOne >> doubleEven) 5
+  print $ execState (doubleEven >> addOne) 5
+  print $ execState (doubleEven >> addOne) 6
--- src/Lib.hs
+++ src/Lib.hs
@@ -4,23 +4,48 @@ import Control.Monad.Trans.Class
 import Control.Monad.Trans.State
 import Control.Monad.Trans.Except
 
+import Data.Char
+
 type App e s a = ExceptT e (StateT s IO) a
 
 runApp :: App e s a -> s -> IO (Either e a, s)
 runApp app s = runStateT (runExceptT app) s
 
-increment :: Monad m => StateT Int m ()
-increment = do
-  modify (+ 1)
+evalApp :: App e s a -> s -> IO (Either e a)
+evalApp app s = evalStateT (runExceptT app) s
+
+incState :: Monad m => StateT Int m ()
+incState = modify succ
 
 printState :: StateT Int IO ()
-printState = do
-  n <- get
-  lift $ print n
+printState = get >>= lift . print
 
-perhap :: Monad m => Int -> ExceptT String m Bool
-perhap 0 = throwE "zero!"
-perhap n = return True
+doubleNonZero :: Monad m => ExceptT String (StateT Int m) ()
+doubleNonZero = do
+  n <- lift get
+  case n of
+    0 -> throwE "zero!"
+    n -> lift . put $ n * 2
+
+echo :: (MonadTrans t, Monad (t IO)) => ExceptT String (t IO) ()
+echo = do
+  lift . lift $ putStr "type 'haskell': "
+  cs <- lift . lift $ getLine
+  if cs == "haskell"
+    then lift . lift $ putStrLn "yes, haskell!"
+    else throwE "boo, not haskell"
+
+tellNumber :: ExceptT String (StateT Int IO) ()
+tellNumber = do
+    lift . lift $ putStr "tell a number: "
+    s <- lift . lift $ getLine
+    if isNumbers s
+      then lift . put $ read s
+      else throwE "not number"
+  where isNumbers = all isNumber
 
 logging :: String -> IO ()
 logging s = putStrLn $ "log: " ++ s
+
+fool :: IO ()
+fool = putStrLn "hey! You fool!!"
